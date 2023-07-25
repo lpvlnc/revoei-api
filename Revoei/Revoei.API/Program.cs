8f7de4f15af238d4b6b757b76eab967219ae050d
Leonardo Valença <lpvalenca@hotmail.com>
2023-07-25 02:21:40 -0300

    Created all the class libraries that the project will need - Added additional information in the swagger page

---
+++ Revoei/Revoei.API/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Revoei API",
+        Description = @"An ASP.NET Core web API for managing Revoei
+                        <br/>
+                        Worked on this project:
+                        <br/>
+                        <a href=""https://www.linkedin.com/in/lpvalenca/"" target=""_blank"">Leonardo Valença - Lead software engineer</a>",
+    });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
